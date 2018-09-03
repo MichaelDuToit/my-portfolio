ad78d53270b8864f0f930df27a72b77bb5ef5d2c
Michael Du Toit <michael.dtoit@gmail.com>
2018-09-04 10:22:29 +1200

    Added Favicons + manifest files, along with linking to them in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,20 @@ const autoprefixer = require("gulp-autoprefixer");
 const runSequence = require("run-sequence");
 const del = require("del");
 
-gulp.task('del', ()=>{
-    return del('build');
+gulp.task("del", ()=>{
+    return del("build");
 });
 
 gulp.task("copy", ()=>{
-    return gulp.src(["src/*.+(html|js)", "src/img/**"], {
+    return gulp.src(["src/*.+(html|js|svg)", "src/img/**"], {
         base: 'src'
     })
     .pipe(gulp.dest("build"))
 });
+gulp.task("copy_favicons", ()=>{
+    return gulp.src("src/favicon/*")
+        .pipe(gulp.dest("build/"))
+})
 
 gulp.task("css", ()=>{
     return gulp.src("src/styles.scss")
@@ -23,5 +27,5 @@ gulp.task("css", ()=>{
 });
 
 gulp.task("build", (callback)=>{
-    runSequence('del', ['copy', 'css'], callback)
+    runSequence("del", ["copy", "copy_favicons", "css"], callback)
 });
